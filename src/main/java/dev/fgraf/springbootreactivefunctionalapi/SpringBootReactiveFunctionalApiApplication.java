85924a4d8d6844a7a0524dc96a11dd19a6513ad4
fgraf <francisco.graf@gmail.com>
2022-07-05 11:36:16 -0400

    Initial commit

---
+++ src/main/java/dev/fgraf/springbootreactivefunctionalapi/SpringBootReactiveFunctionalApiApplication.java
@@ -0,0 +1,13 @@
+package dev.fgraf.springbootreactivefunctionalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveFunctionalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactiveFunctionalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/fgraf/springbootreactivefunctionalapi/SpringBootReactiveFunctionalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fgraf.springbootreactivefunctionalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactiveFunctionalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
